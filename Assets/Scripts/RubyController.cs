499fdb8ebdc72d93decdcb6bc14cf09eed4d5b58
DanS <DSumbillo@colum.edu>
2019-09-28 17:05:18 -0500

    Edited Ruby Controller
    Added horizontal and vertical movement and made it frame independent

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,8 +13,16 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        /// Calls Unity's Horizontal and Vertical GetAxis function
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        /// Moves Ruby in a frame independent manner
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
+
     }
 }
